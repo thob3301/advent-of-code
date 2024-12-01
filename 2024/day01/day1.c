9dcda0c98c9697e7b8fdc4b22ad15c9eea8cc782
Miguel Nunes <59ekim@gmail.com>
2024-12-01 20:23:34 +0000

    day 1 done

--- 2024/day01/day1.c
+++ 2024/day01/day1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
@@ -30,9 +29,9 @@ void	sort(int *arr, size_t n)
 
 int	main(void)
 {
-	int		sum = 0, d1, d2;
+	int		sum_p1 = 0, sum_p2 = 0, d1, d2, repeated;
 	int		*arr1 = NULL, *arr2 = NULL;
-	size_t	arr_size = 0;
+	size_t	arr_size = 0, arr_i = 0, arr_i1 = 0, arr_i2 = 0;
 
 	while (EOF != fscanf(stdin, "%d   %d\n", &d1, &d2))
 	{
@@ -44,9 +43,20 @@ int	main(void)
 	}
 	sort(arr1, arr_size);
 	sort(arr2, arr_size);
-	while (arr_size--)
-		sum += abs(arr1[arr_size] - arr2[arr_size]);
+	while (arr_i++ < arr_size)
+		sum_p1 += abs(arr1[arr_i-1] - arr2[arr_i-1]);
+	while (arr_i1 < arr_size)
+	{
+		repeated = 0;
+		arr_i2 = 0;
+		while (arr_i2 < arr_size)
+			if (arr1[arr_i1] == arr2[arr_i2++])
+				++repeated;
+		sum_p2 += arr1[arr_i1] * repeated;
+		++arr_i1;
+	}
 	free(arr1);
 	free(arr2);
-	printf("%d\n", sum);
+	printf("Part one result: %d\n", sum_p1);
+	printf("Part two result: %d\n", sum_p2);
 }
