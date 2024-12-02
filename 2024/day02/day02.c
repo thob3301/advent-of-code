808fb16335c38a7176f34dbe17bda8838b7966d1
Miguel Nunes <59ekim@gmail.com>
2024-12-02 10:29:17 +0000

    in the middle of day 2 part 1

---
+++ 2024/day02/day02.c
@@ -0,0 +1,13 @@
+int	main(void)
+{
+	int		safe = 0, num;
+	char	tok = NULL;
+	char	*line = NULL;
+	size_t	n = 0;
+
+	while (EOF != getline(&line, &n, stdin))
+	{
+		tok = strtok(line, " ");
+		while (tok)
+		{
+			num = atoi(tok);
