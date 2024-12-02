c7e4443dc22cc720f64ed3f6db3c3c5263c2363b
Miguel Teixeira Nunes <59ekim@gmail.com>
2024-12-02 15:51:18 +0000

    passing it over to pc

--- 2024/day02/day02.c
+++ 2024/day02/day02.c
@@ -1,13 +1,23 @@
 int	main(void)
 {
 	int		safe = 0, num;
+	int		*arr = NULL;
 	char	tok = NULL;
 	char	*line = NULL;
-	size_t	n = 0;
+	size_t	n = 0, arr_size;
 
 	while (EOF != getline(&line, &n, stdin))
 	{
 		tok = strtok(line, " ");
+		arr_size = 0;
 		while (tok)
 		{
 			num = atoi(tok);
+			++arr_size;
+			arr = realloc(arr, arr_size * sizeof(int));
+			arr[arr_size-1] = num;
+			tok = strtok(NULL, " ");
+		}
+
+	}
+}
