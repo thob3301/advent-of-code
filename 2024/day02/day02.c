7351d5c28a80cd973ca88ab0f1d02484144804c2
Miguel Nunes <59ekim@gmail.com>
2024-12-04 00:44:28 +0000

    day 2 part 1 now works, but part 2 is having some issues

--- 2024/day02/day02.c
+++ 2024/day02/day02.c
@@ -4,16 +4,42 @@
 #include <string.h>
 #define UP true
 #define DOWN false
+//		if (arr[arr_i] == arr[arr_i+1])
+//			continue ;
+
+void	safety_check(int *arr, size_t arr_size, bool *safe)
+{
+	size_t	arr_i = 0;
+	bool	trend;
+
+	if (arr[arr_i] < arr[arr_i+1])
+		trend = UP;
+	else
+		trend = DOWN;
+	*safe = true;
+	while (++arr_i < arr_size)
+	{
+		if ((abs(arr[arr_i-1] - arr[arr_i]) <= 3)
+			&& (arr[arr_i-1] != arr[arr_i])
+			&& (trend?(arr[arr_i-1] < arr[arr_i]):(arr[arr_i-1] > arr[arr_i])))
+			continue ;
+		else
+		{
+			*safe = false;
+			break ;
+		}
+	}
+}
 
 int	main(void)
 {
 	int		num;
-	int		*arr = NULL;
-	int		safe_sum = 0;
+	int		*arr = NULL, *arr2 = NULL;
+	int		safe_sum_p1 = 0, safe_sum_p2 = 0;
 	char	*tok = NULL;
 	char	*line = NULL;
-	size_t	n = 0, arr_size, arr_i;
-	bool	safe, trend;
+	size_t	n = 0, arr_size, arr_i, arr_i2, arr_i_neglect;
+	bool	safe;
 
 	while (EOF != getline(&line, &n, stdin))
 	{
@@ -27,30 +53,36 @@ int	main(void)
 			arr[arr_size-1] = num;
 			tok = strtok(NULL, " ");
 		}
-		arr_i = 0;
-		if (arr[arr_i] == arr[arr_i+1])
-			continue ;
-		if (arr[arr_i] < arr[arr_i+1])
-			trend = UP;
-		else
-			trend = DOWN;
-		safe = true;
-		while (arr_i++ < arr_size)
+		safety_check(arr, arr_size, &safe);
+		if (safe)
 		{
-			if ((abs(arr[arr_i-1] - arr[arr_i]) <= 3)
-				&& (arr[arr_i-1] != arr[arr_i])
-				&& (trend?(arr[arr_i-1] < arr[arr_i]):(arr[arr_i-1] > arr[arr_i])))
-				continue ;
-			else
+			++safe_sum_p1;
+			continue ;
+		}
+		arr2 = realloc(arr2, (arr_size-1) * sizeof(int));
+		arr_i_neglect = 0;
+		while (arr_i_neglect < arr_size)
+		{
+			arr_i = 0;
+			arr_i2 = 0;
+			while (arr_i < arr_size)
 			{
-				safe = false;
+				if (arr_i_neglect != arr_i)
+					arr2[arr_i2++] = arr[arr_i];
+				++arr_i;
+			}
+			++arr_i_neglect;
+			safety_check(arr2, arr_size, &safe);
+			if (safe)
+			{
+				++safe_sum_p2;
 				break ;
 			}
 		}
-		if (safe)
-			++safe_sum;
 	}
 	free(arr);
+	free(arr2);
 	free(line);
-	printf("Part one, number of safe reports: %d\n", safe_sum);
+	printf("Part one, number of safe reports: %d\n", safe_sum_p1);
+	printf("Part two, number of safe reports: %d\n", safe_sum_p1+safe_sum_p2);
 }
